4e4568e1a3176b8cac7f38916225e8666a3aa68b
Josecj11 <josecallejimenez11@gmail.com>
2025-02-26 17:13:24 +0100

    prueba

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Init Script");
+let express = require("Express");
+
+let app = express();
+
+
+app.get("/",(request, response)=> {
+    response.send("Hello from the server!")
+});
+
+app.listen(16078);
+
+console.log("Server runnning!");
